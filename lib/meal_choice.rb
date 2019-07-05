34f88b58148efd0091f4523ff892abcc9b4619cd
Timothy Ragan <timothyragan7@gmail.com>
2019-07-05 22:02:35 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -10,12 +10,9 @@ p"A plate of #{protein} with #{veg1} and #{veg2}."
  
 end
 meal_choice("broccoli","macaroni")
-
-meal_choice("broccoli","macaroni")
-
+puts
 
 
-meal_choice("broccoli","macaroni")
 
 
 
