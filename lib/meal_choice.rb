6010111bd052d10d5e08ee1581a705e0e812d7f9
Timothy Ragan <timothyragan7@gmail.com>
2019-07-05 20:37:11 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -1,15 +1,16 @@
 def meal_choice(veg1, veg2, protein="meat")
   puts 'What a nutritious meal!'
   p  "A plate of #{protein} with #{veg1} and #{veg2}."
+
 end
 meal_choice("d","d","d")
 
 def meal_choice_optional(veg1, veg2, protein= "meat")
   puts 'What a nutritious meal!'
- 
+  meal_choice_optional("c","c","c")
   p  "A plate of #{protein} with #{veg1} and #{veg2}." 
 end
-  meal_choice_optional("c","c","c")
+ 
 
 
 
