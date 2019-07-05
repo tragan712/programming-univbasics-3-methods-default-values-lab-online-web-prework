a616114718305c7b3dd6ba2b2032a8ee2923ac7e
Timothy Ragan <timothyragan7@gmail.com>
2019-07-05 21:41:59 +0000

    Automatically backed up by Learn

--- spec/meal_choice_spec.rb
+++ spec/meal_choice_spec.rb
@@ -1,5 +1,20 @@
 require 'spec_helper'
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 describe '#meal_choice' do
   it 'should default to meat for the protein' do
     expect(meal_choice("broccoli", "macaroni")).to eq("A plate of meat with broccoli and macaroni.")
